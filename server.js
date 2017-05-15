367a1af05ad0cf5aec7922d8da80b73012d8a8e9
Anish Prabhu <prabhuanish@berkeley.edu>
2017-05-15 01:21:35 -0700

    Basic REST API complete

---
+++ server.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var time = require('./time');
+var port = process.env.PORT || 8080;
+
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.pre(restify.pre.sanitizePath());
+server.use(function(req, res, next) {
+    console.log(req.method + ' ' + req.url);
+    return next();
+});
+server.use(restify.bodyParser());
+
+server.post('/time/set/:id/:UTCtime', time.post);
+server.post('/time/set/:id', time.post);
+server.get('/time/get/:id/:time_zone', time.getByID);
+server.get('/time/get/:id/:time_zone1/:time_zone2', time.getByID);
+server.del('/time/delete/:id', time.del);
+
+server.listen(port, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
---
+++ time.js
@@ -0,0 +1,109 @@
+var firebase = require("firebase");
+
+// Configure firebase
+var config = {
+    apiKey: "AIzaSyB0oxarY3aGc4I7749K5nBXvoS61rnblzQ",
+    authDomain: "node-restify-demo.firebaseapp.com",
+    databaseURL: "https://node-restify-demo.firebaseio.com",
+    projectId: "node-restify-demo",
+    storageBucket: "node-restify-demo.appspot.com",
+    messagingSenderId: "563332249024"
+};
+firebase.initializeApp(config);
+
+function TimeController() {
+
+    // Get a reference to the database service
+    var database = firebase.database();
+
+    function writeTimeObject(req_id, req_time) {
+      firebase.database().ref('id/' + req_id.toString()).set({
+        id: req_id,
+        time: req_time.toString()
+      });
+    }
+
+    var parseTimeZone = function(req) {
+        if (req.params.time_zone) {
+            return req.params.time_zone;
+        } else if (req.params.time_zone1 && req.params.time_zone2) {
+            return req.params.time_zone1 + "/" + req.params.time_zone2;
+        }
+    } 
+
+    this.getByID = function(req, res, next) {
+
+        var req_id = parseInt(req.params.id);
+
+        if (!isNaN(req_id)) {
+            var ref = firebase.database().ref('id/' + req_id.toString());
+            ref.once("value")
+              .then(function(snapshot) {
+                if (snapshot.exists()) {
+                    var entry = snapshot.val();
+                    console.log(entry);
+                    var time = new Date(Date.parse(entry.time));
+                    var zone = parseTimeZone(req);
+                    console.log("time zone: " + zone);
+                    try {
+                        time = time.toLocaleString('en-US', { timeZone: zone});
+                        res.send(200, {req_id, time});
+                    } catch (e) {
+                        res.send(400, "Error: Invalid Time Zone Provided.");
+                    }
+                    res.send(200, entry);  
+
+                } else {
+                    res.send(400, "Error: ID not found.");
+                }
+
+              });
+        } else {
+            res.send(400, "Error: Please provide a valid ID.");
+        }     
+    }
+
+    this.post = function(req, res, next) {
+        // Check for a valid post request
+        var req_id = parseInt(req.params.id);
+        var req_time = parseInt(req.params.UTCtime);
+
+        if (!isNaN(req_id)) {
+            // Check if UTCtime was provided, otherwise default to current time
+            if (!isNaN(req_time)) {
+                console.log("Req Time: " + req_time.toString());
+                req_time = new Date(req_time);
+            } else {
+                req_time = new Date(new Date().getTime());
+            }
+
+            writeTimeObject(req_id, req_time);
+
+            res.send(200, "Success: Created entry " + req_id.toString() + " with time = " + req_time.toString());
+
+        } else {
+            res.send(400, "Error: Please provide a valid ID.");
+        }
+    }
+
+    this.del = function(req, res, next) {
+        var req_id = parseInt(req.params.id);
+        if (!isNaN(req_id)) {
+            var ref = firebase.database().ref('id/' + req_id.toString());
+            ref.once("value").then(function(snapshot) {
+                if (snapshot.exists()) {
+                    console.log(entry);
+                    var entry = snapshot.val();
+                    ref.remove();
+                    res.send(200, "Success: Time object id " + req_id.toString() + " was removed.");  
+                } else {
+                    res.send(400, "Error: Entry does not exist.");
+                }
+            });
+        } else {
+            res.send(400, "Error: Please provide a valid ID.");
+        }
+    }
+}
+
+module.exports = new TimeController();
